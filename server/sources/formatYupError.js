1f06c35345b2c64a6aea3035cfddd93735f37cbb
Veljko28 <charlibear28@gmail.com>
2022-03-29 16:50:10 +0200

    adding creating a function to format yup errors

---
+++ server/sources/formatYupError.js
@@ -0,0 +1,13 @@
+const formatYupError = (err) => {
+    const errors = [];
+    err.inner.forEach(e => {
+      errors.push({
+        path: e.path,
+        message: e.message
+      });
+    });
+  
+    return errors;
+  };
+
+  module.exports = formatYupError;
