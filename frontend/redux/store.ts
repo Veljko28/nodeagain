a3d8d9b9eed2b329e4f00ebe7427cff02f13ea7a
Veljko28 <charlibear28@gmail.com>
2022-03-30 22:02:43 +0200

    adding the redux store

---
+++ frontend/redux/store.ts
@@ -0,0 +1,9 @@
+import {combineReducers, createStore} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const rootReducer = combineReducers({
+});
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
