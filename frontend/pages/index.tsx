327926d497a54d33d36a79191f9d8349d4f40443
Veljko28 <charlibear28@gmail.com>
2022-04-01 23:35:10 +0200

    adding the custom title chnage component and changing the title

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Grid } from '@mui/material';
 import Sidebar from '../component/Sidebar';
+import TitleChange from '../component/TitleChange';
 import TweetForm from '../component/TweetForm';
 
 export default function Home() {
   return (
     <Grid container>
+       <TitleChange title="Home / Twitter" />
       <Grid xs={2} item/>
 
       <Grid xs={1} item>
