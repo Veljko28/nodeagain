39bf67930c0e1d3ff12971d295bbc13c25e29b7f
Veljko28 <charlibear28@gmail.com>
2022-04-02 21:01:09 +0200

    applying the layout to the app file

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import Layout from '../component/General/Layout';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#A00000'
+    }
+  }
+});
+  
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+      </Provider>
+    </ThemeProvider>
+)
 }
 export default MyApp
