eaf0566a3f150867dd95227146138b818958a2cb
Veljko28 <charlibear28@gmail.com>
2022-04-03 13:24:38 +0200

    making the home page posts show with map

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,39 @@
 import { Grid } from '@mui/material';
-import Sidebar from '../component/General/Sidebar';
 import TitleChange from '../component/TitleChange';
-import TweetForm from '../component/TweetForm';
+import TweetDisplay from '../component/TweetDisplay';
+
+
+const posts = [
+  { username: "Username",
+    at: "username",
+    time: "1h",
+    text: "This is a tweet text that is going to be displayed in the tweet display component", 
+    image: "../public/flag-of-ukraine.png",
+    comments: 8,
+    retweets: 17,
+    likes: 31
+  },
+  { username: "Username",
+    at: "username",
+    time: "1h",
+    text: "This is a tweet text that is going to be displayed in the tweet display component", 
+    image: "../public/flag-of-ukraine.png",
+    comments: 8,
+    retweets: 17,
+    likes: 31
+  }
+]
+
+// map array of posts to array of TweetDisplay components
+const tweetDisplay = posts.map((post, index) => {
+  return <TweetDisplay key={index} {...post} />
+})
 
 export default function Home() {
   return (
     <Grid container>
        <TitleChange title="Home / Twitter" />
-       <TweetForm/>
+       {tweetDisplay}
     </Grid>
   )
 }
