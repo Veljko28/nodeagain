7b857f1dc078be0fcc52c5bfea16796f881fee21
Veljko28 <charlibear28@gmail.com>
2022-04-03 13:22:19 +0200

    adding custom interface for tweet display

---
+++ frontend/component/customInterfaces/PostInterface.tsx
@@ -0,0 +1,10 @@
+export interface PostInterface {
+  username: string;
+  at: string,
+  time: string,
+  text: string,
+  image: string,
+  likes: number,
+  comments: number,
+  retweets: number
+}
