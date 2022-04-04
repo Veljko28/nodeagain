4ba5a36e161eb02a03ac35039c9d2d3f386aa154
Veljko28 <charlibear28@gmail.com>
2022-04-04 18:12:09 +0200

    making changes to tweet display from codeforces

--- frontend/component/TweetDisplay.tsx
+++ frontend/component/TweetDisplay.tsx
@@ -13,66 +13,70 @@ import { PostInterface } from './customInterfaces/PostInterface';
 
 const TweetDisplay = ({username, at, time, text, image, comments, retweets, likes } : PostInterface) => {
   return (
-    <Grid container className="hoverFx" style={{maxWidth: 500, border: '1px solid #eee', padding: 10}}>
+    <Grid container className="hoverFx" style={{maxWidth: 600, border: '1px solid #eee', padding: 10}}>
       <Grid item container xs={12}>
         
-        <Grid item xs={2}>
+        <Grid item xs={1}>
           <Avatar alt="Avatar" className="hoverFx"
-          sx={{ width: 50, height: 50, cursor: "pointer", display: "inline-block", margin: 1 }}
+          sx={{ width: 55, height: 55, cursor: "pointer", display: "inline-block", marginTop: 1 }}
           src="https://i.pinimg.com/originals/d1/47/e9/d147e940041c8a9a6a4cdf8116271171.jpg" />
       
         </Grid>
 
-        <Grid item xs={9} style={{marginTop: 25}}>
-            <span style={{fontWeight:'bold', cursor: 'pointer'}}>{username}</span>
-            <span style={{cursor: "pointer"}}> @{at} · {time}</span>
+        <Grid item container xs={11} style={{marginTop: 15}}>
+          
+          <Grid item xs={10} style={{ paddingLeft: 12}}>
+              <span style={{fontWeight:'bold', cursor: 'pointer'}}>{username}</span>
+              <span style={{cursor: "pointer"}}> @{at} · {time}</span>
+          </Grid>
+
+          <Grid item xs={1}>
+            <IconButton color="primary">
+              <MoreHorizIcon />
+            </IconButton>
+          </Grid>
+
+
+          <Grid item xs={11} >
+            <Typography variant="subtitle1" style={{paddingLeft: 10, maxWidth: 600}}>
+                {text}
+            </Typography>
+          </Grid>
+
+          <Grid item xs={1} />
+          <Grid item xs={11} style={{paddingLeft:10}}>
+            <Image src="/flag-of-ukraine.jpg" width="450px" height="250px" style={{borderRadius: 10}}/>
+          </Grid>
+
+          <Grid item xs={1}  />
+          <Grid item container xs={11} style={{paddingLeft: 10,marginTop: 10}}>
+            <Grid xs={3} item>
+              <IconButton style={{color: "#2196f3"}}>
+                  <ChatBubbleOutlineIcon style={{fontSize: 17}}/>
+              </IconButton>
+              {comments}
+            </Grid>
+            <Grid xs={3} item>
+              <IconButton style={{color: "#00a152"}}>
+                  <LoopIcon style={{fontSize: 17}}/>
+              </IconButton>
+              {retweets}
+            </Grid>
+            <Grid xs={4} item>
+              <IconButton color="primary">
+                  <FavoriteBorderIcon style={{fontSize: 17}}/> 
+              </IconButton>
+              {likes}
+            </Grid>
+
+              <Grid xs={2} item>
+              <IconButton style={{color: "#2196f3"}}>
+                  <FileUploadIcon style={{fontSize: 20}}/>
+              </IconButton>
+            </Grid>
+          </Grid>
         </Grid>
 
-        <Grid item xs={1}>
-          <IconButton color="primary">
-            <MoreHorizIcon />
-          </IconButton>
-        </Grid>
-
-
-        <Grid item xs={11} >
-          <Typography variant="subtitle1" style={{paddingLeft: 10, maxWidth: 600}}>
-              {text}
-          </Typography>
-        </Grid>
-
-        <Grid item xs={1} />
-        <Grid item xs={11} style={{paddingLeft:10}}>
-          <Image src="/flag-of-ukraine.jpg" width="450px" height="250px" style={{borderRadius: 10}}/>
-        </Grid>
-
-        <Grid item xs={1}  />
-        <Grid item container xs={11} style={{paddingLeft: 10,marginTop: 10}}>
-           <Grid xs={3} item>
-             <IconButton style={{color: "#2196f3"}}>
-                <ChatBubbleOutlineIcon style={{fontSize: 17}}/>
-             </IconButton>
-             {comments}
-           </Grid>
-           <Grid xs={3} item>
-             <IconButton style={{color: "#00a152"}}>
-                <LoopIcon style={{fontSize: 17}}/>
-             </IconButton>
-             {retweets}
-           </Grid>
-           <Grid xs={4} item>
-             <IconButton color="primary">
-                <FavoriteBorderIcon style={{fontSize: 17}}/> 
-             </IconButton>
-             {likes}
-           </Grid>
-
-            <Grid xs={2} item>
-             <IconButton style={{color: "#2196f3"}}>
-                <FileUploadIcon style={{fontSize: 20}}/>
-             </IconButton>
-           </Grid>
-        </Grid>
       </Grid>
     </Grid>
   )
