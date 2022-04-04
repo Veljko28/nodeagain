ccd35e42cbf4c4abecee8060a5ede9b54e3b9eef
Veljko28 <charlibear28@gmail.com>
2022-04-04 18:28:49 +0200

    adding sidebar update

--- frontend/component/General/Sidebar.tsx
+++ frontend/component/General/Sidebar.tsx
@@ -23,11 +23,7 @@ const Title = ({name} : {name: string}) => {
 
 const Sidebar = () => {
   return (
-<<<<<<< HEAD
      <Grid container>
-=======
-     <>
->>>>>>> 5e62a3931fe82b774931f17384af3ce3fd0caf88
     <List component="nav" style={{maxWidth: 200}}>
        <Link href="/">
          <IconButton onClick={() => {}} style={{marginLeft: 10}} disableTouchRipple >
@@ -91,7 +87,6 @@ const Sidebar = () => {
       </ListItem>
    </Link>
 
-<<<<<<< HEAD
       <Button disableTouchRipple style={{backgroundColor: 'red', borderRadius: '500px', padding: 15, width: 225, marginLeft: 5}}>
         <Typography variant="h6" style={{marginLeft: 5, color: 'white', fontSize: 15}}> Tweet</Typography>
       </Button>
@@ -111,106 +106,7 @@ const Sidebar = () => {
             <div>
                <div style={{fontWeight:'bold', cursor: 'pointer'}}>Username</div>
                <span style={{cursor: "pointer"}}> @username</span>
-            </div>import { Grid, Button, IconButton, Typography, List, ListItem } from '@mui/material'
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import SvgLogo from '../../public/logo.svg';
-
-import HomeIcon from '@mui/icons-material/Home';
-import TagIcon from '@mui/icons-material/Tag';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import EmailIcon from '@mui/icons-material/Email';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import ListAltIcon from '@mui/icons-material/ListAlt';
-import PersonIcon from '@mui/icons-material/Person';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-
-
-
-const Title = ({name} : {name: string}) => {
-  return (
-    <Typography variant="h6" style={{marginLeft: 5}}> {name}</Typography>
-  )
-}
-
-const Sidebar = () => {
-  return (
-     <>
-    <List component="nav" style={{maxWidth: 200}}>
-       <Link href="/">
-         <IconButton onClick={() => {}} style={{marginLeft: 10}} disableTouchRipple >
-            <Image src={SvgLogo} width="30px" height="30px" />
-         </IconButton>
-       </Link>
-
-       <Link href="/">
-         <ListItem button disableRipple onClick={() => {}} style={{borderRadius: '500px', color: 'black', margin: 10, display: ''}}>
-            <HomeIcon fontSize='medium'/> 
-            <Title name="Home"/>
-         </ListItem>
-       </Link>
-
-      <Link href="/explore">
-         <ListItem button disableRipple onClick={() => {}} style={{borderRadius: '500px', color: 'black', margin: 10}}>
-            <TagIcon fontSize='medium'/> 
-            <Title name="Explore"/>
-         </ListItem>
-      </Link>
-
-   <Link href="/notifications">
-      <ListItem button disableRipple onClick={() => {}} style={{borderRadius: '500px', color: 'black', margin: 10}}>
-         <NotificationsIcon fontSize='medium'/> 
-         <Title name="Notifications"/>
-      </ListItem>
-   </Link>
-
-   <Link href="/messages">
-      <ListItem button disableRipple onClick={() => {}} style={{borderRadius: '500px', color: 'black', margin: 10}}>
-         <EmailIcon fontSize='medium'/> 
-         <Title name="Messages"/>
-      </ListItem>
-   </Link>
-
-   <Link href="/bookmarks">
-      <ListItem button disableRipple onClick={() => {}} style={{borderRadius: '500px', color: 'black', margin: 10}}>
-         <BookmarkIcon fontSize='medium'/> 
-         <Title name="Bookmarks"/>
-      </ListItem>
-   </Link>
-
-   <Link href="/lists">
-      <ListItem button disableRipple onClick={() => {}} style={{borderRadius: '500px', color: 'black', margin: 10}}>
-         <ListAltIcon fontSize='medium'/> 
-         <Title name="Lists"/>
-      </ListItem>
-   </Link>
-
-   <Link href="/profile">
-      <ListItem button disableRipple onClick={() => {}} style={{borderRadius: '500px', color: 'black', margin: 10}}>
-         <PersonIcon fontSize='medium'/>
-         <Title name="Profile"/>
-      </ListItem>
-   </Link>
-
-   <Link href="/more">
-      <ListItem button disableRipple onClick={() => {}} style={{borderRadius: '500px', color: 'black', margin: 10}}>
-         <MoreHorizIcon fontSize='medium'/> 
-         <Title name="More"/>
-      </ListItem>
-   </Link>
-
-      <Button disableTouchRipple style={{backgroundColor: 'red', borderRadius: '500px', padding: 15, width: 250, marginLeft: 5}}>
-        <Typography variant="h6" style={{marginLeft: 5, color: 'white', fontSize: 15}}> Tweet</Typography>
-      </Button>
-    </List>
-    
-    
-    </>
-  )
-}
-
-export default Sidebar
+            </div>
          </Grid>
          <Grid item xs={2} style={{marginTop: 15}}>
             <IconButton color="primary">
@@ -220,15 +116,6 @@ export default Sidebar
       </Grid>
     
     </Grid>
-=======
-      <Button disableTouchRipple style={{backgroundColor: 'red', borderRadius: '500px', padding: 15, width: 250, marginLeft: 5}}>
-        <Typography variant="h6" style={{marginLeft: 5, color: 'white', fontSize: 15}}> Tweet</Typography>
-      </Button>
-    </List>
-    
-    
-    </>
->>>>>>> 5e62a3931fe82b774931f17384af3ce3fd0caf88
   )
 }
 
