d6816ea6737ad0d12d432ef1ef7ed88001a2ef87
Veljko28 <charlibear28@gmail.com>
2022-04-04 18:15:29 +0200

    adding unfinished profile page

---
+++ frontend/pages/profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TweetDisplayMap } from '../component/General/TweetDisplayMap';
+import ProfileHead from '../component/ProfileHead';
+
+
+const profile = () => {
+  return (
+    <>
+    <ProfileHead/>
+    <TweetDisplayMap/>
+    </>
+  )
+}
+
+export default profile
