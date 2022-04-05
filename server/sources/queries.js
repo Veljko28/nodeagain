a1dc0ced88301bfb73c4ee719fdaf61793292b86
Veljko28 <charlibear28@gmail.com>
2022-04-05 15:44:37 +0200

    adding new includes to queries

--- server/sources/queries.js
+++ server/sources/queries.js
@@ -1,10 +1,8 @@
 const Pool = require('pg').Pool;
-const pool = new Pool({
+export const pool = new Pool({
     user: 'artisoft',
     host: 'localhost',
     database: 'userproj',
     password: process.env.POSTGRES_PASSWORD,
     port: 5432
 })
-
-module.exports = pool;
