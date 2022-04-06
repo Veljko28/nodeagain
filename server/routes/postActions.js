34f93a9d846c30022910c05f61eaeb903a8fc501
Veljko28 <charlibear28@gmail.com>
2022-04-06 21:19:51 +0200

    making new post actions for favorite, retweet and share

---
+++ server/routes/postActions.js
@@ -0,0 +1,17 @@
+import {v4} from 'uuid';
+import express from 'express';
+import {pool} from '../sources/queries';
+
+const router = express.Router();
+
+
+router.post('/favorite',(req, res) => {
+    const { postId, userId } = req.body;
+    pool.query("INSERT INTO favorites(postId, userId) VALUES ($1,$2)",
+     [postId, userId] ,(err,result) => {
+        if (err) throw err;
+        res.json(result.rowCount == 1);
+    })
+});
+
+
