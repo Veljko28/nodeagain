13948defe6c5c8b486fe2e7cd35ff45f913b0d3b
Veljko28 <charlibear28@gmail.com>
2022-04-06 21:18:55 +0200

    making small adjusments to posts router

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/like/:txt', (req,res) => {
     const txt = req.params.id;
-    pool.query("SELECT * FROM posts WHERE text LIKE $1", [txt],(err,result) => {
+    pool.query("SELECT * FROM tweets WHERE text LIKE $1", [txt],(err,result) => {
         if (err) throw err;
         res.status(200).json(result.rows);
     })
@@ -19,14 +19,14 @@ router.get('/like/:txt', (req,res) => {
 
 router.route('/:id').get((req,res) => {
     const id = req.params.id;
-    pool.query("SELECT * FROM posts WHERE id = $1", [id] ,(err,result) => {
+    pool.query("SELECT * FROM tweets WHERE id = $1", [id] ,(err,result) => {
         if (err) throw err;
         if (!result.rowCount) res.status(404).json(false);
         else res.json(result.rows[0]);
     })
 }).delete((req, res ) => {
     const id = req.params.id;
-    pool.query("DELETE FROM posts WHERE id = $1", [id] ,(err,result) => {
+    pool.query("DELETE FROM tweets WHERE id = $1", [id] ,(err,result) => {
         if (err) throw err;
         res.json(result.rows);
     })
@@ -35,7 +35,7 @@ router.route('/:id').get((req,res) => {
 router.post('/create',(req, res) => {
     const { text,userId,mediaPath,timeCreated } = req.body;
     const id = v4();
-    pool.query("INSERT INTO posts(id,text,userId,mediaPath,timeCreated) VALUES ($1,$2,$3,$4,$5)",
+    pool.query("INSERT INTO tweets(id,text,userId,mediaPath,timeCreated) VALUES ($1,$2,$3,$4,$5)",
      [id,text,userId,mediaPath,timeCreated] ,(err,result) => {
         if (err) throw err;
         res.json(result.rowCount == 1);
