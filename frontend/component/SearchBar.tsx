a81adcbbd51d3531b29faf2b04e3a5bbf836b581
Veljko28 <charlibear28@gmail.com>
2022-04-08 13:41:26 +0200

    adding the search bar

--- frontend/component/SearchBar.tsx
+++ frontend/component/SearchBar.tsx
@@ -1,8 +1,20 @@
+import { IconButton, InputBase, Paper } from '@mui/material'
 import React from 'react'
+
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <Paper style={{width: 350, borderRadius: 25, backgroundColor: '#fafafa', padding: 2}}>
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 2, mr: 2, width: '75%' }}
+        placeholder="Search Twitter"
+        inputProps={{ 'aria-label': 'search twitter' }}
+      />
+    </Paper>
   )
 }
 
