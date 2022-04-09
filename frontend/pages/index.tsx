6db44639acb3371614e8eeea88c930e4c2c0fcf7
Veljko28 <charlibear28@gmail.com>
2022-04-09 18:40:53 +0200

    adding search bar to home page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Grid } from '@mui/material';
 import { TweetDisplayMap } from '../component/General/TweetDisplayMap';
 import TitleChange from '../component/TitleChange';
+import TweetForm from '../component/TweetForm';
 
 export default function Home() {
   return (
     <Grid container>
        <TitleChange title="Home / Twitter" />
+       <TweetForm/>
        <TweetDisplayMap/>
     </Grid>
   )
