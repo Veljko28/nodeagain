5bf27ea1393ac4f975a5d37a0b984f62b40bae9a
Veljko28 <charlibear28@gmail.com>
2022-04-15 17:18:39 +0200

    adding comment actions to routes

--- server/routes/postActions.js
+++ server/routes/postActions.js
@@ -58,5 +58,29 @@ router.post('/retweet', (req,res) => {
     })
 })
 
+router.route('/comment').get((req,res) => {
+    const {postId} = req.body;
+    pool.query("SELECT * FROM comments WHERE postId = $1", [postId], (err,result) => {
+        if (err) throw err;
+        res.status(200).json(result.rows);
+    })
+})
+.post((req,res) => {
+    const {postId,userId,text,timeCreated} = req.body;
+    const id = v4();
+    pool.query("INSERT INTO comments(id,postId,userId,text,timeCreated) VALUES ($1,$2,$3,$4,$5)",
+    [id,postId,userId,text,timeCreated], (err,result) => {
+        if (err) throw err;
+        res.status(201).json(true);
+    })
+})
+.delete((req,res) => {
+    const {id} = req.body;
+    pool.query("DELETE FROM comments WHERE id = $1",[id], (err,result) => {
+        if (err) throw err;
+        res.status(202).json(true);
+    })
+})
+
 module.exports = router;
 
